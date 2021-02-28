5c1157d7d6888f0b857f0ed6d481fe3946a7c1bc
Rafael Moreno <Rafael Moreno@LAPTOP-ROGBR6G0>
2021-02-27 20:46:25 -0600

    tarea1

---
+++ src/operadorespromedio60066/OperadoresPromedio60066.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorespromedio60066;
+
+/**
+ *
+ * @author Rafael Moreno
+ */
+public class OperadoresPromedio60066 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num_1,num_2,num_3,num_4,num_5;
+        num_1 = 14;
+        num_2 = 45;
+        num_3 = 15;
+        num_4 = 19;
+        num_5 = 87;
+        int promedio = ((num_1+num_2+num_3+num_4+num_5)/5);
+        System.out.println("EL promedio es: "+ promedio);
+    }
+    
+}
